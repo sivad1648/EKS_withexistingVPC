78f96da27044eea69a65832c3656162a0dfc5920
“sivad1648” <sivad1648@gmail.com>
2022-06-15 16:23:13 +0530

    changes to subnet_ids is eks.tf and removed variable file

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "devopsthehardway-eks" {
  role_arn = aws_iam_role.eks-iam-role.arn
 
  vpc_config {
-  subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+  subnet_ids = [module.education-vpc.public_subnets, module.education-vpc.private_subnets]
  }
 
  depends_on = [
--- workernodes.tf
+++ workernodes.tf
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "worker-node-group" {
   depends_on = [
    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+   aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
  }
 

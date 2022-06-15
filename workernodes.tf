afe50f22228523df8cf0c3921fc00b92810b8e04
“sivad1648” <sivad1648@gmail.com>
2022-06-15 15:13:14 +0530

    second commit

--- workernodes.tf
+++ workernodes.tf
@@ -6,8 +6,8 @@ resource "aws_eks_node_group" "worker-node-group" {
   instance_types = ["t2.micro"]
  
   scaling_config {
-   desired_size = 1
-   max_size   = 1
+   desired_size = 2
+   max_size   = 3
    min_size   = 1
   }
  

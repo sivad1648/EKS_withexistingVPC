37749c3e32d109ca2a2a052cea7a696cbc9b7acd
“sivad1648” <sivad1648@gmail.com>
2022-06-15 16:23:40 +0530

    remove variable file

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subnet_id_1" {
-  type = string
-  default = "module.education-vpc.public_subnets"
- }
- 
- variable "subnet_id_2" {
-  type = string
-  default = "module.education-vpc.private_subnets"
- }

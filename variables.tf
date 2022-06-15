ce5e4a38a4f303bb2d5031b991b559afdc1ff3d0
“sivad1648” <sivad1648@gmail.com>
2022-06-15 15:58:10 +0530

    change in subnet

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-03b80680309532864"
+  default = "subnet-0ba3886e2d8efda0a"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "subnet-07745bc1fe9efd62e"
+  default = "subnet-090302c7b3c9721ef"
  }
